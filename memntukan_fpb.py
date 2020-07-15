f0aae47e5f327e72b972e66c08703aff9473a120
adikusuma <adikusuma1402@gmail.com>
2020-07-15 22:33:03 +0700

    menambahkan cara mencari fpb denfan algoritma euclidean

--- memntukan_fpb.py
+++ memntukan_fpb.py
@@ -1,21 +1,33 @@
-def hitung_fpb(x, y):
-    ''' fungsi untuk menentukan fpb dari 2 bilangan  '''
-
-    if x > y:
-        smaller = y
-    else:
-        smaller = x
-
-    for i in range(1, smaller+1):
-        if x % i== 0 and y % i == 0:
-            fpb = i
-
-    return fpb
-
-# num1 = 96
-# num2 = 24
-
+# def hitung_fpb(x, y):
+#     ''' fungsi untuk menentukan fpb dari 2 bilangan  '''
+#
+#     if x > y:
+#         smaller = y
+#     else:
+#         smaller = x
+#
+#     for i in range(1, smaller+1):
+#         if x % i== 0 and y % i == 0:
+#             fpb = i
+#
+#     return fpb
+#
+# # num1 = 96
+# # num2 = 24
+#
 num1 = int(input('Masukan bilangan pertama: '))
 num2 = int(input('Masukan bilangan kedua: '))
 
+#menggunakan algoritma euclidean
+
+def hitung_fpb(x, y):
+    ''' fungsi untuk menentukan fpb dari dua bilangan dengan algoritma euclidean '''
+
+    while(y):
+        x, y = y, x % y
+        return x
+
+
+# num1 = 200
+# num2 = 150
 print(f'fpb dari {num1} dan {num2} = {hitung_fpb(num1, num2)}')
