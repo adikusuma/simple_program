ae9a1a779bd5503fc252b773d4671899ef19ae6c
adikusuma <adikusuma1402@gmail.com>
2020-07-16 22:37:13 +0700

    menambahkan program range bilangan prima

---
+++ range_bil-prima.py
@@ -0,0 +1,13 @@
+''' menentukan bilangan prima dari dengan range tertentu '''
+
+low = 200
+upp = 300
+
+for num in range(low, upp+1):
+    if num > 1:
+        for i in range(2, num):
+            if num % i == 0:
+                break
+
+        else:
+            print(num)
